1b185333f5e5622f2282ccc11d7f846bb5bab0a6
Lucas Henrique De França de Meneses <lucas.henrique.fm99@gmail.com>
2026-04-23 11:12:39 -0300

    teste1

--- cypress/e2e/divisao/cenarios/divisao.cy.js
+++ cypress/e2e/divisao/cenarios/divisao.cy.js
@@ -2,25 +2,26 @@ import { Given, When, Then, And } from 'cypress-cucumber-preprocessor/steps';
 
 
 Given('estou dentro do sistema Calculator', () => {
-  cy.visit('https://testpages.herokuapp.com/calculate.php');
+  cy.visit('https://www.calculator.net/');
 });
 
-When('digitar o numero 10 na primeira caixa', () =>{
-  cy.get('#number1').type('10')
+When('digitar o numero 10 da calculadora', () =>{
+  cy.get('r(1)').click()
+  cy.get('r(0)').click()
 });
 
-Then('alterar plus para divide', () =>{
-  cy.get('#function').select('divide');
-});
+// Then('alterar plus para divide', () =>{
+//   cy.get('#function').select('divide');
+// });
 
-And('digitar 2 na segunda caixa', () =>{
-  cy.get('#number2').type('2')
-});
+// And('digitar 2 na segunda caixa', () =>{
+//   cy.get('#number2').type('2')
+// });
 
-And('clicar no botão Calcular', () =>{
-  cy.get('#calculate').click()
-});
+// And('clicar no botão Calcular', () =>{
+//   cy.get('#calculate').click()
+// });
 
-Then('deve mostrar que o resultado é 5', () => {
-  cy.get('#answer').should('have.text', '5')
-})
+// Then('deve mostrar que o resultado é 5', () => {
+//   cy.get('#answer').should('have.text', '5')
+// })
