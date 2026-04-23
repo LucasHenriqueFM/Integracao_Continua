44ae7fbb4dfa2fb9e1dbcf6709ffb061f71a0583
Lucas Henrique De França de Meneses <lucas.henrique.fm99@gmail.com>
2026-04-23 10:50:25 -0300

    alteração de teste na soma

--- cypress/e2e/soma/cenarios/soma.cy.js
+++ cypress/e2e/soma/cenarios/soma.cy.js
@@ -5,18 +5,18 @@ Given('estou dentro do sistema Calculator', () => {
   cy.visit('https://testpages.herokuapp.com/calculate.php');
 });
 
-When('digitar o numero 4 na primeira caixa', () =>{
-  cy.get('#number1').type('4')
+When('digitar o numero 5 na primeira caixa', () =>{
+  cy.get('#number1').type('5')
 });
 
-And('digitar 6 na segunda caixa', () =>{
-  cy.get('#number2').type('6')
+And('digitar 10 na segunda caixa', () =>{
+  cy.get('#number2').type('10')
 });
 
 And('clicar no botão Calcular', () =>{
   cy.get('#calculate').click()
 });
 
-Then('deve mostrar que o resultado é 10', () => {
-  cy.get('#answer').should('have.text', '10')
+Then('deve mostrar que o resultado é 15', () => {
+  cy.get('#answer').should('have.text', '15')
 })
--- cypress/e2e/soma/soma.feature
+++ cypress/e2e/soma/soma.feature
@@ -2,7 +2,7 @@ Feature: Calculator
 
 Scenario: soma
     Given estou dentro do sistema Calculator
-    When digitar o numero 4 na primeira caixa
-    And digitar 6 na segunda caixa
+    When digitar o numero 5 na primeira caixa
+    And digitar 10 na segunda caixa
     And clicar no botão Calcular
-    Then deve mostrar que o resultado é 10
+    Then deve mostrar que o resultado é 15
